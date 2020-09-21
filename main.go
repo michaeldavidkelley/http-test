b9eaf9ce475ac1d59a4d13989092cae8dd88e096
Jason Heimann <jheimann@digitalocean.com>
2020-09-21 15:28:15 -0700

    add skeleton code

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net"
+)
+
+func handleConnection(c net.Conn) {
+	// do something interesting
+}
+
+func main() {
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		// handle error
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			// handle error
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
+
